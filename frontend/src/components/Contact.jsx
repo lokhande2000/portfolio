8323d0929e30612fa4913f15cc1c76b99fdbe57e
suraj <surajlokhande7744@gmail.com>
2024-08-05 09:07:21 +0530

    portfolio

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -6,29 +6,31 @@ const Contact = () => {
     <Box
       id="contactus"
       className="contact_section"
-      p={{ base: 6, md: 12 }}
-      overflow="hidden"
+      p={{ base: 0, sm: 12 }}
+      // overflow="hidden"
     >
       <VStack
         className="container"
         alignItems="flex-start"
-        flexWrap="wrap"
+        // flexWrap="wrap"
         spacing={5}
         maxW="container.md"
+        minW={{ base: "100%", md: "container.md" }}
         mx="auto"
+        p={{ base: 2, sm: 6, md: 12 }}
       >
         <Heading
           as="h2"
           size={{ base: "lg", md: "xl" }}
-          textAlign={{ base: "center", md: "left" }}
+          // textAlign={{ base: "center", md: "left" }}
           w="full"
         >
           Get in Touch
         </Heading>
         <Text
           fontSize={{ base: "sm", sm: "xl" }}
-          textAlign={{ base: "center", md: "left" }}
-          w="full"
+          // textAlign={{ base: "center", md: "left" }}
+          minW={{ base: "full", md: "80%" }}
         >
           Whether you want to get in touch, talk about a project collaboration,
           or just say hi, I'd love to hear from you.
@@ -38,8 +40,9 @@ const Contact = () => {
             w="full"
             className="flexitem"
             alignItems="center"
-            flexDirection={{ base: "column", md: "row" }}
-            textAlign={{ base: "center", md: "left" }}
+            // flexDirection={{ base: "column", md: "row" }}
+            // textAlign={{ base: "center", md: "left" }}
+            fontSize={{ base: "sm", sm: "xl" }}
           >
             <Icon as={FaPhone} m={3} />
             <Text>+91 7447300432</Text>
@@ -48,8 +51,9 @@ const Contact = () => {
             w="full"
             className="flexitem"
             alignItems="center"
-            flexDirection={{ base: "column", md: "row" }}
-            textAlign={{ base: "center", md: "left" }}
+            // flexDirection={{ base: "column", md: "row" }}
+            // textAlign={{ base: "center", md: "left" }}
+            fontSize={{ base: "sm", sm: "xl" }}
           >
             <Icon as={FaGithub} m={3} />
             <Link
@@ -64,8 +68,9 @@ const Contact = () => {
             w="full"
             className="flexitem"
             alignItems="center"
-            flexDirection={{ base: "column", md: "row" }}
-            textAlign={{ base: "center", md: "left" }}
+            // flexDirection={{ base: "column", md: "row" }}
+            // textAlign={{ base: "center", md: "left" }}
+            fontSize={{ base: "sm", sm: "xl" }}
           >
             <Icon as={FaLinkedin} m={3} />
             <Link
@@ -80,8 +85,9 @@ const Contact = () => {
             w="full"
             className="flexitem"
             alignItems="center"
-            flexDirection={{ base: "column", md: "row" }}
-            textAlign={{ base: "center", md: "left" }}
+            // flexDirection={{ base: "column", md: "row" }}
+            // textAlign={{ base: "center", md: "left" }}
+            fontSize={{ base: "sm", sm: "xl" }}
           >
             <Icon as={FaEnvelope} m={3} />
             <Link id="contact-email" href="mailto:surajlokhande7744@gmail.com">
